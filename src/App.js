d42ec71b49fca90bd1461fbe0322e25f4dc686d1
atulawari <atulawari14@gmail.com>
2024-12-10 01:18:17 +0530

    till 20

--- src/App.js
+++ src/App.js
@@ -7,60 +7,83 @@ import Alert from "./Components/Alert";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState("light"); // Whether dark mode is active or not
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type) => {
-    setAlert({
-      msg: message,
-      type: type,
-    });
-    setTimeout(() => {
-      setAlert(null);
-    }, 1500);
-  };
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
 
-  const toggleMode = () => {
-    if (mode === "light") {
-      setMode("dark");
-      document.body.style.backgroundColor = "grey";
-      showAlert("Dark mode has been enable", "success");
-      document.title = "My First App - Dark Mode";
-    } else {
-      setMode("light");
-      document.body.style.backgroundColor = "white";
-      showAlert("Light mode has been enable", "success");
-      document.title = "My First App - Light Mode";
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
     }
-  };
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
 
-  return (
-    <>
-      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-      {/* <Navbar/> */}
-      <Router>
-        <Navbar title="ATUL" mode={mode} toggleMode={toggleMode} />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Routes>
-            {/* /users --> Component 1
-        /users/home --> Component 2 */}
-            <Route exact path="/about" element={<About />} />
-            <Route
-              exact path="/"
-              element={
-                <TextForm
-                  showAlert={showAlert}
-                  heading="Enter the text to analyze below"
-                  mode={mode}
-                />
-              }
-            />
-          </Routes>
-        </div>
-      </Router>
-    </>
-  );
-}
+//   return (
+//     <>
+//         <Navbar title="ATUL" mode={mode} toggleMode={toggleMode} />
+//         <Alert alert={alert} />
+        
+//         <div className="container my-3">
+//                 <TextForm
+//                   showAlert={showAlert}
+//                   heading="Enter the text to analyze below"
+//                   mode={mode}
+//                 />
+                
+//         </div>
+//         <div className="container my-3">
+//         <About/>
+//         </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+return (
+ <>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    {/* <Navbar/> */}
+    <Router>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+      <Routes>
+          {/* /users --> Component 1
+      /users/home --> Component 2 */}
+          <Route exact path="/about" element={<About />} />
+          <Route
+            exact path="/"
+            element={
+              <TextForm
+                 showAlert={showAlert}
+                heading="Enter the text to analyze below"
+                mode={mode}
+              />
+             }
+         />
+         </Routes>
+      </div>
+    </Router>
+   </>
+ );
+ }
 
 export default App;
